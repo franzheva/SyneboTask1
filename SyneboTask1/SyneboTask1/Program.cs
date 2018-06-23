1a4ad5648c5f19ca194d2b8b37124de155c2fa97
franzheva <40060461+franzheva@users.noreply.github.com>
2018-06-23 23:02:15 +0300

    Add files via upload

---
+++ SyneboTask1/SyneboTask1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SyneboTask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Input A:");
+                int A = Int32.Parse(Console.ReadLine());
+                Console.WriteLine("Input B (must be bigger than A):");
+
+                int B = Int32.Parse(Console.ReadLine());
+
+                if (A <= B)
+                {
+                    int C = Count(A, B);
+                    Console.WriteLine($"This interval has {C} whole squares");
+                }
+                else
+                {
+                    Console.WriteLine("Wrong input! Press any key to exit");
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            Console.ReadKey();
+        }
+        static int Count(int a, int b)
+        {
+            int length = b - a;
+            int c = 0;
+
+            for (int i = 0; i <= length; i++)
+            {
+                if ((Math.Sqrt(a) % 1) == 0)
+                    c++;
+                a++;
+            }
+            return c;
+
+        }
+
+    }
+}
